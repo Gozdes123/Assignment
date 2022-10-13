c8638e36e462e2bdcd42ccd09f72c2fd05a2004d
WilsonCheng <reborn2616am@gmail.com>
2022-10-13 08:17:51 +0800

    1013

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import store from "./store";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "./assets/tailwind.css";
+import "./assets/tailwind.css";
 createApp(App).use(store).use(router).mount("#app");
